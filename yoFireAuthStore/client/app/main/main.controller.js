198ba0d2df2adf8764d6e54c12deb28e80ca4ebc
Scott Nakada <scottnakada@gmail.com>
2015-12-24 12:15:06 -0800

    Fix some links.

--- yoFireAuthStore/client/app/main/main.controller.js
+++ yoFireAuthStore/client/app/main/main.controller.js
@@ -8,22 +8,22 @@
         constructor($scope, $state, Auth, toaster) {
 
             /* Pull in the current user information from the Auth Factory */
-            $scope.currentUser = Auth.user;
+            this.currentUser = Auth.user;
 
             /* Return true if the user is signed in */
-            $scope.signedIn = Auth.signedIn;
+            this.signedIn = Auth.signedIn;
 
             /* Return true if the user has Manager permissions (or Admin) */
-            $scope.isManager = Auth.isManager;
+            this.isManager = Auth.isManager;
 
             /* Return true if the user has Admin permissions */
-            $scope.isAdmin = Auth.isAdmin;
+            this.isAdmin = Auth.isAdmin;
 
             /* Return true if the user is a simple pass user, so changing password is ok */
-            $scope.changePassOk = Auth.changePassOk;
+            this.changePassOk = Auth.changePassOk;
 
             /* Handle Logout */
-            $scope.logout = function () {
+            this.logout = function () {
                 toaster.pop('success', "Logged out successfully");
                 Auth.logout();
                 $state.go('main');
